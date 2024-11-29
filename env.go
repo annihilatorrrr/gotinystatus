06df5b0b4c580463e15a87134ca199e672ed5791
CGenie <cgenie@pm.me>
2024-11-29 21:59:44 +0100

    Update to reflect tinystatus .yaml file (#13)
    * Fix things so that it is compatible with tinystatus groups
    
    * [log] remove Groups log
    
    * [template] fix template rendering (use variables)
    
    * [server] fix history.html url
    
    * [sync] fix ordering of groups and results
    
    * remove GOMAXPROCS setting

---
+++ env.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/gomarkdown/markdown"
+	"github.com/joho/godotenv"
+	"github.com/sethvargo/go-envconfig"
+	"gopkg.in/yaml.v3"
+	"log"
+	"os"
+	"path"
+)
+
+type Config struct {
+	CheckInterval       int    `env:"CHECK_INTERVAL, default=60"`
+	MaxHistoryEntries   int    `env:"MAX_HISTORY_ENTRIES, default=10"`
+	ChecksFile          string `env:"CHECKS_FILE, default=checks.yaml"`
+	IncidentsFile       string `env:"INCIDENTS_FILE, default=incidents.html"`
+	HistoryFile         string `env:"HISTORY_FILE, default=history.json"`
+	HtmlOutputDirectory string `env:"HTML_OUTPUT_DIRECTORY, default=status"`
+	Port                int    `env:"PORT"`
+	Token               string `env:"TOKEN"`
+	Chatid              string `env:"CHATID"`
+}
+
+func readEnv() Config {
+	godotenv.Load()
+
+	ctx := context.Background()
+
+	var ret Config
+
+	if err := envconfig.Process(ctx, &ret); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := os.MkdirAll(ret.HtmlOutputDirectory, 0755); err != nil {
+		log.Fatal(err)
+	}
+
+	return ret
+}
+
+func (c *Config) PrintEnv() {
+	fmt.Println("---------------------")
+	fmt.Printf("CHECK_INTERVAL=%d\n", c.CheckInterval)
+	fmt.Printf("MAX_HISTORY_ENTRIES=%d\n", c.MaxHistoryEntries)
+	fmt.Printf("CHECKS_FILE=%s\n", c.ChecksFile)
+	fmt.Printf("INCIDENTS_FILE=%s\n", c.IncidentsFile)
+	fmt.Printf("STATUS_HISTORY_FILE=%s\n", c.HistoryFile)
+	fmt.Printf("HTML_OUTPUT_DIRECTORY=%s\n", c.HtmlOutputDirectory)
+	fmt.Printf("PORT=%d\n", c.Port)
+	fmt.Printf("TOKEN=%s\n", c.Token)
+	fmt.Printf("CHATID=%s\n", c.Chatid)
+	fmt.Println("---------------------")
+}
+
+func (c *Config) IndexHtmlFile() string {
+	return path.Join(c.HtmlOutputDirectory, "index.html")
+}
+
+func (c *Config) HistoryHtmlFile() string {
+	return path.Join(c.HtmlOutputDirectory, "history.html")
+}
+
+func (c *Config) ListenHost() string {
+	return fmt.Sprintf(":%d", c.Port)
+}
+
+func (c *Config) ReadIncidentHtml() []byte {
+	incidentMarkdown, err := os.ReadFile(c.IncidentsFile)
+	if err != nil {
+		log.Println("Failed to load incidents:", err)
+		incidentMarkdown = []byte("## All Fine!")
+	}
+	return markdown.ToHTML(incidentMarkdown, nil, nil)
+}
+
+func (c *Config) ReadChecks() []Group {
+	checksData, err := os.ReadFile(c.ChecksFile)
+	if err != nil {
+		log.Fatal("Failed to load checks file:", err)
+	}
+	var groups []Group
+	if err = yaml.Unmarshal(checksData, &groups); err != nil {
+		log.Fatal("Failed to parse checks file:", err)
+	}
+
+	return groups
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module gotinystatus
 go 1.23.0
 
 require gopkg.in/yaml.v3 v3.0.1
+
+require (
+	github.com/gomarkdown/markdown v0.0.0-20241105142532-d03b89096d81 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/sethvargo/go-envconfig v1.1.0 // indirect
+)
